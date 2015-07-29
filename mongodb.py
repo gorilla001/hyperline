7bfe9db49cdb7c8c0949535ed386aa64456980c9
niuminguo <niuminguo@shequcun.com>
2015-07-29 10:51:25 +0800

    add connect mongodb retry

--- mongodb.py
+++ mongodb.py
@@ -4,10 +4,16 @@ __author__ = 'nmg'
 __all__ = ['MongoProxy']
 
 from pymongo import MongoClient
+import pymongo.errors.ConnectionFailure
+import log as logging
+import asyncio
+import sys
 
 _MONGO_HOST = '192.168.99.100'
 _MONGO_PORT = 32768
 _MONGO_DB = 'hyperline'
+
+logger = logging.getLogger(__name__)
 
 class MongoProxy(object):
     def __init__(self):
@@ -15,10 +21,36 @@ class MongoProxy(object):
         self.port = _MONGO_PORT
         self.db = _MONGO_DB
         self.connection = None
+        self.max_retries = 5
+        self.retry_interval = 5
 
         self.connect()
 
     def connect(self):
+        # attempt = 0
+        # while True:
+        #     attempt += 1
+        #     try:
+        #         self.connection = MongoClient(self.host, self.port)
+        #     except pymongo.errors.ConnectionFailure as exc:
+        #         if attempt >= self.max_retries:
+        #             logger.error("Connected mongodb error: {}".format(exc))
+
+        attempt = 0
+        while True:
+            attempt += 1
+            try:
+                self._connect()
+                return
+            except pymongo.errors.ConnectionFailure:
+                logger.info("Connecting to MongoDB failed...retry")
+
+            if attempt >= self.max_retries:
+                logger.error("Connecting to MongoDB server on %(host)s:%(port)d falied" % (self.host, self.port))
+                sys.exit(1)
+            yield from asyncio.sleep(self.retry_interval)
+
+    def _connect(self):
         self.connection = MongoClient(self.host, self.port)
 
     def save_msg(self, msg, coll='messages'):
